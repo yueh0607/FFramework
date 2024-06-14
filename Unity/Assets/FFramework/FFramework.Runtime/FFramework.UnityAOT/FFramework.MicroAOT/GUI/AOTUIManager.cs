240f441fa49d8444ee621ac7629695a2bea7aa09
yzp <imyueh@foxmail.com>
2024-06-14 11:26:28 +0800

    AOT UIMagr

--- Unity/Assets/FFramework/FFramework.Runtime/FFramework.UnityAOT/FFramework.MicroAOT/GUI/AOTUIManager.cs
+++ Unity/Assets/FFramework/FFramework.Runtime/FFramework.UnityAOT/FFramework.MicroAOT/GUI/AOTUIManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using YooAsset;
 
 namespace FFramework
@@ -9,32 +10,50 @@ namespace FFramework
     public class AOTUIManager : AOTSingleton<AOTUIManager>
     {
         private Dictionary<ValueTuple<string, int>, AOTPanel> m_Panels = new Dictionary<(string, int), AOTPanel>();
-        public IEnumerator Open(string location,int key=0) 
+
+        private Camera m_UICamera;
+        private EventSystem m_EventSystem;
+        private Transform m_PanelRoot;
+
+        public void Initialize(Camera uiCamera,EventSystem eventSystem,Transform PanelRoot)
         {
+            this.m_PanelRoot = PanelRoot;
+            this.m_UICamera = uiCamera;
+            this.m_EventSystem = eventSystem;
+        }
+
+
+        public IEnumerator Open(string location, int key = 0)
+        {
+            if (m_Panels.ContainsKey(new ValueTuple<string, int>(location, key)))
+                throw new InvalidOperationException($"Panel {location}:{key} has opened");
+
             var panelPrefab = YooAssets.LoadAssetAsync<GameObject>(location);
             yield return panelPrefab;
             var ins = panelPrefab.InstantiateAsync();
             yield return ins;
 
+            panelPrefab.Release();
+
             if (ins.Result.TryGetComponent(out AOTPanel panel))
             {
+                m_Panels.Add((location, key), panel);
                 panel.Location = location;
                 panel.Key = key;
-
-      
-                panel.OnShow();
+                yield return panel.OnShow();
             }
             else throw new System.ArgumentNullException($"{location}:{key} has not a {nameof(AOTPanel)} Component");
-
         }
 
-        public void Focus(string location) 
-        {
 
-        }
-
-        public void Close()
+        public IEnumerator Close(string location, int key)
         {
+            if (!m_Panels.ContainsKey(new ValueTuple<string, int>(location, key)))
+                throw new InvalidOperationException($"Panel {location}:{key} has not opened");
+
+            var panel = m_Panels[(location, key)];
+            yield return panel.OnClose();
+            GameObject.Destroy(panel.gameObject);
 
         }
     }
